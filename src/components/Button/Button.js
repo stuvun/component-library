bf4ef57a38b39bada4355d972d711cef6f95389d
Steven Kim <sskim14@gmail.com>
2019-10-30 18:44:55 -0400

    Set up storybook component library

---
+++ src/components/Button/Button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Button.css'
+
+// This is a functional component - just sent up a little differently as an arrow function!
+const Button = (props) => {
+    let classList = ''
+    let types = [ 'primary', 'danger', 'success', 'warning' ]
+
+    if (types.includes(props.type)) {        // Conditionals for what style is returned based on property
+        classList += ` button-${props.type}`
+    }
+    if (props.large) {
+        classList += ` button-large`
+    }
+    return ( <button className={classList}>
+                {props.label}
+             </button> )
+} 
+
+export default Button;
---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Button from './Button';
+
+storiesOf('Button', module)
+    .add('Primary', () => <Button
+        label="Primary"
+        type="primary"
+    />)
+    .add('Danger', () => <Button
+        label="Danger"
+        type="danger"
+    />)
+    .add('Success', () => <Button
+        label="Success"
+        type="success"
+    />)
+    .add('Warning', () => <Button
+        label="Warning"
+        type="warning"
+    />)
+    .add('Large Primary', () => <Button
+        label="Large"
+        type="primary"
+        large
+    />)
